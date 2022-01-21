9532df4960c0d5f1c8e5a657acb5daab27037052
caetanoti <47917105+caetanoti@users.noreply.github.com>
2022-01-20 22:01:04 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase';	
 
 const firebaseConfig = {	
-     apiKey: $"VAR_API",
-  authDomain: $"VAR_AUT",
-  projectId: $"VAR_PROJECT",
-  storageBucket: $"VAR_STORAGE",
-  messagingSenderId: $"VAR_MESS",
-  appId: $"VAR_APP"
+ -apiKey: $"VAR_API",
+ -authDomain: $"VAR_AUT",
+ -projectId: $"VAR_PROJECT",
+ -storageBucket: $"VAR_STORAGE",
+ -messagingSenderId: $"VAR_MESS",
+ -appId: $"VAR_APP"
+  
 };	
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);	
